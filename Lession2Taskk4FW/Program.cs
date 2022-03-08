aded4d83b629594258764508c0681023f0d8e2b1
Ivan <kiba82@gmail.com>
2022-03-08 09:23:39 +0300

    Начал выполнение ДЗ 3й урок

---
+++ Lession2Taskk4FW/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lession2Taskk4FW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
