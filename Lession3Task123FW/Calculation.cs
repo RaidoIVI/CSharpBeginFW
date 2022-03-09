eabc5ae2eb16f76b0753752728421d13c7676b53
Иван Жеребятьев <kiba82@gmail.com>
2022-03-09 20:50:43 +0300

    Основное сделал. Доделать: 1.Вывод из программы. 2.Страховка от некорретного ввода в номере телефона/почте. 3. Рассмотреть вывод из телефонной книжки.

---
+++ Lession3Task123FW/Calculation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace Lession3Task123FW
+{
+    internal partial class Program
+    {
+        class Calculation
+        {
+            internal static string Mirror(string value)
+            {
+                if (value == null) { return value; }
+                var tmp = new StringBuilder(value.Length);
+                for (int i = 0; i < value.Length; i++) { tmp.Append(value[((value.Length - 1) - i)]); }
+                return Convert.ToString(tmp);
+            }
+            internal static string Diagonal (int [,] value1, bool value2) 
+            {
+                var tmp = new StringBuilder(value1.GetLength(0));
+               
+                    for (int i = 0; i < value1.GetLength(0) ; i++)
+                    {
+                        for (int j = 0; j < value1.GetLength(1) ; j++)
+                        {
+                            if ((i == j) && value2) { tmp.Append(value1[i, j]); }
+                            else if ((i+j+1 == value1.GetLength(0) ) && !value2) { tmp.Append(value1[i, j]); }
+                        }                 
+                    }
+               return Convert.ToString(tmp);
+            }
+
+
+
+
+
+        }
+    }
+}
---
+++ Lession3Task123FW/Get.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Lession3Task123FW
+{
+    internal partial class Program
+    {
+        class Get 
+        {
+            internal static string String (string value)
+            {
+                Console.Write(value);
+                string tmp = Console.ReadLine();
+                return tmp;
+            }
+            internal static string [,] PhoneBook (int value)
+            {
+                string [,]  tmp = new string [value,2];
+                for (int i = 0; i < tmp.GetLength (0) ; i++) 
+                {
+                    tmp[i,0] = Get.String("Введите имя: ");
+                    tmp[i, 1] = Get.String("Ведите номер телефона/e-mail: ");
+                }
+                return tmp;
+            }
+            internal static int[,] Array (int value) 
+            {
+                Random rnd = new Random();
+                int[,] tmp = new int [value,value];
+                for (var i = 0; i < tmp.GetLength(0) ; i++) 
+                {
+                    for (var j = 0; j < tmp.GetLength(1) ; j++) 
+                    {
+                        tmp[i, j] = rnd.Next(10);
+                    }
+                }
+                return tmp;
+            }
+            
+
+        }
+
+
+    }
+}
