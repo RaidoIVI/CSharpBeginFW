b23ed0afce06d83d1df6a8beddf8f9d436bc6c7d
Ivan <kiba82@gmail.com>
2022-04-12 18:12:46 +0300

    Выполнено задание 2

---
+++ Lession5Task1_2_3FW/Program.cs
@@ -0,0 +1,10 @@
+﻿using Lession5Task1_2_3FW;
+// Задание 1
+
+
+
+// Задание 2
+StartUp.Start() ;
+
+// Задание 3
+
---
+++ Lession5Task1_2_3FW/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lession5Task1_2_3FW
+{
+    internal static class StartUp
+    {
+        private const string _path = @"d:\ExampleDir";
+        private const string _fileName = "StartUp.txt";
+
+        public static void Start ()
+        {
+            var date = Convert.ToString( DateTime.Now);
+            
+           if ( ! Directory.Exists (_path) )
+            {
+                Directory.CreateDirectory ( _path );
+            }
+            Directory.SetCurrentDirectory ( _path );
+            File.AppendAllText( Path.Combine(_path,_fileName ), date + "\n");
+            
+        }
+    }
+}
